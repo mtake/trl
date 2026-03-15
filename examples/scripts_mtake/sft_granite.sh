f75885fd627181f16535085d1e258c9791274d98
Mikio Takeuchi <mtake@jp.ibm.com>
2026-03-15 10:34:21 +0000

    WIP add script

---
+++ examples/scripts_mtake/sft_granite.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# for macOS
+if command -v gdate &> /dev/null
+then
+    DATE_CMD=gdate
+else
+    DATE_CMD=date
+fi
+
+START_TIME="$(${DATE_CMD} +%s)"
+START_TIME_STR="$(${DATE_CMD} -d @${START_TIME} +%Y%m%d-%H%M%S)"
+BASENAME="$(basename "${BASH_SOURCE}" .sh)"
+HOSTNAME_S="$(hostname -s)"
+LOGFILE="${BASENAME}-${START_TIME_STR}-${HOSTNAME_S}.log"
+echo "XXX LOGFILE ${LOGFILE}" | tee -a ${LOGFILE}
+echo "XXX DATETIME ${START_TIME_STR}" | tee -a ${LOGFILE}
+
+VENV=../../.venv
+if [[ -d "${VENV}" ]]; then
+    source "${VENV}/bin/activate"
+fi
+
+ENV=""
+ENV="TOKENIZERS_PARALLELISM=false ${ENV}"
+ENV="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True ${ENV}"  # deprecated
+ENV="PYTORCH_ALLOC_CONF=expandable_segments:True ${ENV}"
+ENV="NCCL_DEBUG=INFO ${ENV}"
+
+# @@@ahoaho XXX
+if false; then
+#if true; then
+# @@@ahoaho XXX
+ENV="TORCH_CPP_LOG_LEVEL=INFO ${ENV}"
+ENV="TORCH_DISTRIBUTED_DEBUG=DETAIL ${ENV}"
+
+ENV="NCCL_DEBUG_SUBSYS=ALL ${ENV}"
+#ENV="CUDA_LAUNCH_BLOCKING=1 ${ENV}"
+#ENV="TORCH_USE_CUDA_DSA=1 ${ENV}"
+
+ENV="NCCL_ASYNC_ERROR_HANDLING=1 ${ENV}"  # deprecated
+ENV="TORCH_NCCL_ASYNC_ERROR_HANDLING=1 ${ENV}"
+
+#ENV="NCCL_P2P_DISABLE=1 ${ENV}"
+#ENV="NCCL_SHM_DISABLE=1 ${ENV}"
+#ENV="NCCL_IB_DISABLE=1 ${ENV}"
+fi
+
+cmd="${ENV}accelerate launch --config_file ../../examples/accelerate_configs/single_gpu.yaml ${BASENAME}.py"
+echo "$cmd" | tee -a ${LOGFILE}
+eval "$cmd" 2>&1 | tee -a ${LOGFILE}
+
+END_TIME="$(${DATE_CMD} +%s)"
+END_TIME_STR="$(${DATE_CMD} -d @${END_TIME} +%Y%m%d-%H%M%S)"
+echo "XXX DATETIME ${END_TIME_STR}" | tee -a ${LOGFILE}
+echo "XXX ELAPSED_SECS $((END_TIME - START_TIME))" | tee -a ${LOGFILE}
