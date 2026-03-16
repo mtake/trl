ae69b4251c26d03c6a5f3e5d0be67b575009fa65
Mikio Takeuchi <mtake@jp.ibm.com>
2026-03-16 17:44:58 +0900

    WIP granite

--- examples/scripts_mtake/sft_granite.py
+++ examples/scripts_mtake/sft_granite.py
@@ -28,15 +28,21 @@ accelerate launch --config_file examples/accelerate_configs/fsdp2.yaml examples/
 """
 
 from datasets import load_dataset
+from datetime import datetime
+import os
 
 from trl import SFTConfig, SFTTrainer
 
 
 def main():
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    _timestamp = f"__{timestamp}"
+
     data_name = "jfe-technical-report_r5"
+    _data_name = f"__{data_name}" if data_name is not None and len(data_name) > 0 else ""
 
     # Load dataset
-    train_dataset = load_dataset("json", data_files=f"messages_data_{data_name}.jsonl", split="train")
+    train_dataset = load_dataset("json", data_files=f"messages_data{_data_name}.jsonl", split="train")
 
     # Load model
     model_id = "ibm-granite/granite-3.3-8b-instruct"
@@ -44,20 +50,19 @@ def main():
 
     model_id_short = model_id[model_id.rfind("/")+1:]
 
-    output_dir = f"trainer_output__{model_id_short}__{data_name}"
+    output_prefix = "trainer_output"
+    os.makedirs(output_prefix, exist_ok=True)
+    output_dir = f"{output_prefix}/{model_id_short}{_data_name}{_timestamp}"
 
     # Train model
     training_args = SFTConfig(
         output_dir=output_dir,  # default: trainer_output
-        # @@@ahoaho XXX
-        # per_device_train_batch_size=128,  # default: 8 CUDA OOM with max_length=8192 fsdp2_1node_2proc.yaml
         per_device_train_batch_size=32,  # default: 8 OK with max_length=20000 fsdp2_1node_2proc.yaml
         # num_train_epochs=1,  # default: 3
         # gradient_accumulation_steps=8,  # default: 1
         bf16=True,  # default: None
         # use_liger_kernel=True,
         dataset_num_proc=8,  # default: None
-        # max_length=8192,  # default: 1024
         max_length=20000,  # default: 1024
     )
 
