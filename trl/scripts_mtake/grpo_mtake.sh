c59d4fd353db63e6806030ee2086a0f4c11cde67
Mikio Takeuchi <mtake@jp.ibm.com>
2026-04-23 13:51:36 +0000

    WIP grpo

--- trl/scripts_mtake/grpo_mtake.sh
+++ trl/scripts_mtake/grpo_mtake.sh
@@ -112,25 +112,29 @@ ACCELERATE_OPT=""
 #ACCELERATE_OPT="${ACCELERATE_OPT} --offload_optimizer_device cpu"  # for zero_stage>=2  # DPO OK for g4hs
 #ACCELERATE_OPT="${ACCELERATE_OPT} --offload_param_device cpu"  # for zero_stage>=3  # DPO OK for g4hs
 
-#OUTPUT_DIR="trainer_output/${MODEL##*/}-${DATASET_S}-dpo-${START_TIME_STR}-${HOSTNAME_S}"  # NOTE neither timestamp nor hostname works with preemptable queue
-OUTPUT_DIR="trainer_output/${MODEL##*/}-${DATASET_S}-dpo"  # NOTE neither timestamp nor hostname works with preemptable queue
+#OUTPUT_DIR="trainer_output/${MODEL##*/}-${DATASET_S}-grpo-${START_TIME_STR}-${HOSTNAME_S}"  # NOTE neither timestamp nor hostname works with preemptable queue
+OUTPUT_DIR="trainer_output/${MODEL##*/}-${DATASET_S}-grpo"  # NOTE neither timestamp nor hostname works with preemptable queue
 
 echo "================== ENVIRONMENT VARIABLES ===================" | tee -a ${LOGFILE}
 env 2>&1 | tee -a ${LOGFILE}
 echo "============================================================" | tee -a ${LOGFILE}
 
 
-# See https://github.com/mtake/trl/blob/main/trl/scripts/dpo.py
+# See https://github.com/mtake/trl/blob/main/trl/scripts/grpo.py
 cmd="${ENV}accelerate launch --config_file ${ACCELERATE_CONFIG}${ACCELERATE_OPT} ${BASENAME}.py --model_name_or_path ${MODEL}"
 if [[ -n "${USE_CONFIG}" ]]; then
     cmd="$cmd --config ${DATASET}"
 else
     cmd="$cmd --dataset_name ${DATASET}"
 fi
-# @@@ahoaho XXX
-cmd="$cmd --reward_funcs ${REWARD_FUNCS}"
 cmd="$cmd --output_dir ${OUTPUT_DIR}"
-cmd="$cmd --per_device_train_batch_size 1"  # default: 8  # DPO OK for g4hs
+cmd="$cmd --reward_funcs ${REWARD_FUNCS}"
+# @@@ahoaho XXX
+#cmd="$cmd --num_generations 4"  # default: 8. The effective batch size (num_processes * per_device_batch_size * gradient_accumulation_steps) must be evenly divisible by this value.
+#cmd="$cmd --num_generations 8"  # default: 8. The effective batch size (num_processes * per_device_batch_size * gradient_accumulation_steps) must be evenly divisible by this value.  # GRPO OK? for g418b num_processes=4 per_device_train_batch_size=2 num_generations=8
+# @@@ahoaho XXX
+#cmd="$cmd --per_device_train_batch_size 1"  # default: 8  # DPO OK for g4hs
+cmd="$cmd --per_device_train_batch_size 2"  # default: 8  # GRPO OK? for g418b num_processes=4 per_device_train_batch_size=2 num_generations=8
 #cmd="$cmd --num_train_epochs 1"  # default: 3
 cmd="$cmd --save_strategy epoch"  # default: steps
 ####cmd="$cmd --max_steps 10"  # default: -1 (len(train split) * num_train_epochs)
@@ -140,9 +144,7 @@ cmd="$cmd --save_strategy epoch"  # default: steps
 #cmd="$cmd --use_liger_kernel True"  # default: False
 cmd="$cmd --dtype bfloat16"
 cmd="$cmd --bf16 True"
-cmd="$cmd --dataset_num_proc 8"
 #cmd="$cmd --no_remove_unused_columns"  # default: False
-cmd="$cmd --max_length 8192"  # default: 1024  # DPO OK for g418b dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1 dataset_name=datasets/retriever_call_train_data.granite4_8b.jsonl, DPO OK for g4130b dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1 config=zragrtrvr.v2.yaml, DPO OK for g4hs dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1 dataset_name=datasets/retriever_call_train_data.granite4_8b.jsonl
 #cmd="$cmd --eval_strategy steps"  # default: no  # requires test split
 #cmd="$cmd --eval_steps 50"
 #cmd="$cmd --per_device_eval_batch_size 1"  # default: 8
