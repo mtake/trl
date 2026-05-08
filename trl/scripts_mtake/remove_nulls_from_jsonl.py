d296a1ca4d65a3de5bd34e1f307029e3234c22ac
Mikio Takeuchi <mtake@jp.ibm.com>
2026-05-09 02:54:05 +0900

    add script to remove nulls

---
+++ trl/scripts_mtake/remove_nulls_from_jsonl.py
@@ -0,0 +1,44 @@
+import argparse
+import json
+
+def remove_nulls(x):
+    if isinstance(x, dict):
+        return { k: remove_nulls(v) for k, v in x.items() if v is not None }
+    elif isinstance(x, list):
+        return [ remove_nulls(v) for v in x ]
+    else:
+        return x
+
+def main():
+    parser = argparse.ArgumentParser(description="Remove nulls from JSONL file")
+
+    parser.add_argument("input_file", help="Path to the input file")
+    parser.add_argument(
+        "--output", "-o",
+        # default="output.jsonl",
+        help="Path to the output file"
+    )
+    # parser.add_argument(
+    #     "--verbose", "-v",
+    #     action="store_true",
+    #     help="Enable verbose output"
+    # )
+
+    args = parser.parse_args()
+
+    # if args.verbose:
+    #     print("Verbose mode enabled")
+
+    input_file = args.input_file
+    output_file = args.output
+
+    with open(input_file, "r", encoding="utf-8") as fin, open(output_file, "w", encoding="utf-8") as fout:
+
+        for line in fin:
+            obj = json.loads(line)
+            obj = remove_nulls(obj)
+            fout.write(json.dumps(obj, ensure_ascii=False) + "\n")
+
+
+if __name__ == "__main__":
+    main()
