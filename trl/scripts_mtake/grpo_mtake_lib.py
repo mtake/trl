b3ac42249776f6471f25f60ed4f1393e78bdad3e
Mikio Takeuchi <mtake@jp.ibm.com>
2026-05-15 02:36:56 +0900

    clean up

--- trl/scripts_mtake/grpo_mtake_lib.py
+++ trl/scripts_mtake/grpo_mtake_lib.py
@@ -1,19 +1,5 @@
-# from collections.abc import Callable
 from trl.rewards import accuracy_reward
 
-
-# def my_accuracy_reward(
-#     completions: list[list[dict[str, str]]],
-#     solution: list[str],
-#     log_extra: Callable[[str, list], None] | None = None,
-#     **kwargs,
-# ) -> list[float | None]:
-#     return accuracy_reward(
-#         completions=completions,
-#         solution=solution,
-#         log_extra=log_extra,
-#         **kwargs,
-#     )
 
 def my_accuracy_reward(**kwargs):
     return accuracy_reward(**kwargs)
