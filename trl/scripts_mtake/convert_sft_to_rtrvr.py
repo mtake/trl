b63ce75c319e3bd84d1ab8436c517e01a3f0a718
Mikio Takeuchi <mtake@jp.ibm.com>
2026-05-23 04:45:10 +0900

    WIP convert sft to retriever

---
+++ trl/scripts_mtake/convert_sft_to_rtrvr.py
@@ -0,0 +1,93 @@
+import argparse
+import json
+import re
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert sft dataset to retriever data")
+
+    parser.add_argument("input_file", help="Path to the input file")
+    parser.add_argument(
+        "--output", "-o",
+        default="output.json",
+        help="Path to the output file"
+    )
+    parser.add_argument(
+        "--raw", "-r",
+        action="store_true",
+        help="Keep raw documents. Otherwise, convert raw documents to a list"
+    )
+    # parser.add_argument(
+    #     "--verbose", "-v",
+    #     action="store_true",
+    #     help="Enable verbose output"
+    # )
+
+    args = parser.parse_args()
+
+    # if args.verbose:
+    #     print("Verbose mode enabled")
+
+    input_file = args.input_file
+    output_file = args.output
+    raw = args.raw
+
+    rtrvr = {}
+
+    with open(input_file, "r", encoding="utf-8") as fin, open(output_file, "w", encoding="utf-8") as fout:
+
+        for line in fin:
+            obj = json.loads(line)
+            messages = obj["messages"]
+            query = None
+            raw_documents = None
+            for message in reversed(messages):
+                role = message["role"]
+                if not raw_documents:
+                    if role == "tool":
+                        raw_documents = message["content"]
+                    continue
+                if role == "user":
+                    query = message["content"]
+                    break
+
+            if query and raw_documents:
+                # print(f"XXX raw_documents = XXX {raw_documents} XXX")
+
+                if raw:
+                    documents = raw_documents
+                else:
+                    documents = []
+                    # convert raw documents to a list of document
+                    document_split = re.split(r'\[Rank \d+\] ', raw_documents)
+                    if not document_split[0].strip():
+                        document_split = document_split[1:]
+                    # print(f"XXX document_split = XXX {document_split} XXX")
+                    for i, document in enumerate(document_split):
+                        start_title = document.find("Title: ")
+                        start_url = document.find("URL: ")
+                        start_score = document.find("Score: ")
+                        start_content = document.find("Content: ")
+                        title = document[start_title+len("Title: "):start_url].strip()
+                        url = document[start_url+len("URL: "):start_score].strip()
+                        score = document[start_score+len("Score: "):start_content].strip()
+                        content = document[start_content+len("Content: "):]
+                        if content.endswith("\n\n"):
+                            content = content[:-len("\n\n")]
+                        # if content.endswith("\n#\n"):
+                        #     content = content[:-len("\n#\n")]
+                        document_dict = {
+                            "rank": i+1,
+                            "title": title,
+                            "url": url,
+                            "score": float(score),
+                            "content": content,
+                        }
+                        # print(f"XXX document_dict = XXX {document_dict} XXX")
+                        documents.append(document_dict)
+
+                rtrvr[query] = documents
+
+        json.dump(rtrvr, fout, ensure_ascii=False)
+
+if __name__ == "__main__":
+    main()
