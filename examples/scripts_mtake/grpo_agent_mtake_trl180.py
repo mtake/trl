7d21f1f1436399896e39447900e87529082076e3
Mikio Takeuchi <mtake@jp.ibm.com>
2026-07-29 23:17:22 +0900

    clean up grpo_agent_mtake.py

--- examples/scripts_mtake/grpo_agent_mtake_trl180.py
+++ examples/scripts_mtake/grpo_agent_mtake_trl180.py
@@ -266,12 +266,6 @@ def format_example(example):
 if __name__ == "__main__":
     parser = TrlParser((ScriptArguments, GRPOConfig, ModelConfig))
     script_args, training_args, model_args = parser.parse_args_and_config()
-    # @@@ahoaho XXX
-    local_rank = int(os.environ.get("LOCAL_RANK", 0))
-    if local_rank == 0:
-        print(f"XXX script_args: {script_args} XXX")
-        print(f"XXX training_args: {training_args} XXX")
-        print(f"XXX model_args: {model_args} XXX")
 
     # @@@ahoaho XXX
     # # ------------------------
@@ -290,11 +284,15 @@ if __name__ == "__main__":
     #     print(f"biogrid.db created. Rows stored: {len(df)}")
     # finally:
     #     conn.close()
-    # ------------------------
-    # Create DB (rank 0 only to avoid concurrent write conflicts)
-    # ------------------------
-    # local_rank = int(os.environ.get("LOCAL_RANK", 0))
+    local_rank = int(os.environ.get("LOCAL_RANK", 0))
     if local_rank == 0:
+        print(f"XXX script_args: {script_args} XXX")
+        print(f"XXX training_args: {training_args} XXX")
+        print(f"XXX model_args: {model_args} XXX")
+
+        # ------------------------
+        # Create DB (rank 0 only to avoid concurrent write conflicts)
+        # ------------------------
         print("Creating biogrid.db...")
         # Load dataset
         biogrid_dataset = load_dataset("qgallouedec/biogrid", split="train")
