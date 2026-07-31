21cd7fdbf40a3c1c01457b7b7af04165b6e06695
Mikio Takeuchi <mtake@jp.ibm.com>
2026-07-31 11:22:04 +0000

    GRPO AGENT OK for g418b 8gpu

--- examples/scripts_mtake/grpo_agent_mtake.sh
+++ examples/scripts_mtake/grpo_agent_mtake.sh
@@ -85,8 +85,7 @@ MODEL=Qwen/Qwen3-0.6B  # GRPO AGENT OK
 ####MODEL=ibm-granite/granite-4.0-h-small  # may cause OSError due to slow system system
 ####MODEL=models/granite-4.0-h-small
 #MODEL=ibm-granite/granite-4.1-3b  # GRPO AGENT OK for g413b use_vllm=True (see trl/chat_template_utils.py)
-#MODEL=ibm-granite/granite-4.1-8b
-####MODEL=models/granite-4.1-8b
+#MODEL=ibm-granite/granite-4.1-8b  # GRPO AGENT OK for g418b use_vllm=True num_processes=8 per_device_train_batch_size=8 num_generations=8 vllm_gpu_memory_utilization=0.5 vllm_max_model_length=65536
 ####MODEL=trainer_output/granite-4.1-8b-retriever_call_train_data.granite4_8b.v2.0406-SFT-sft-20260409-150354-p4-r26-n3-g418b-3epochs-8192length-rtrvr.v2
 ####MODEL=trainer_output/granite-4.1-8b-retriever_call_train_data.granite4_8b.v2.0406-SFT-sft-20260416-100514-p1-r23-n3-g418b-3epochs-8192length-rtrvr.v2-transformers4576
 #MODEL=models/granite-4.1-30b
@@ -114,7 +113,7 @@ MODEL=Qwen/Qwen3-0.6B  # GRPO AGENT OK
 #ACCELERATE_CONFIG=accelerate_configs/zero3_1node_1proc.yaml
 #ACCELERATE_CONFIG=accelerate_configs/zero3_1node_2proc.yaml  # SFT CUDA OOM for g338b, DPO OK for g4m, g4hm, g4ht dtype=bfloat16, DPO CUDA OOM for g418b dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1
 ACCELERATE_CONFIG=accelerate_configs/zero3_1node_4proc.yaml  # SFT OK for g338b, DPO OK for q205b, g338b, g4m, g4hm, g4ht dtype=bfloat16, DPO CUDA OOM for g4hs dtype=bfloat16, DPO OK for g418b dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1, DPO CUDA OOM for g4130b dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1, GRPO AGENT OK for q306b, GRPO AGENT OK for g413b use_vllm=True
-#ACCELERATE_CONFIG=accelerate_configs/zero3_1node_8proc.yaml  # DPO OK for g338b dtype=bfloat16, DPO CUDA OOM for g4hs dtype=bfloat16, DPO CUDA OOM for g4hs dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1, DPO OK for g4hs offload_optimizer_device=cpu offload_param_device=cpu dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1, DPO OK for g4130b dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1
+#ACCELERATE_CONFIG=accelerate_configs/zero3_1node_8proc.yaml  # DPO OK for g338b dtype=bfloat16, DPO CUDA OOM for g4hs dtype=bfloat16, DPO CUDA OOM for g4hs dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1, DPO OK for g4hs offload_optimizer_device=cpu offload_param_device=cpu dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1, DPO OK for g4130b dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1, GRPO AGENT OK for g418b use_vllm=True num_processes=8 per_device_train_batch_size=8 num_generations=8 vllm_gpu_memory_utilization=0.5 vllm_max_model_length=65536
 ####ACCELERATE_CONFIG=accelerate_configs/zero3_1node_8proc_offload.yaml  # DPO OK for g4hs dtype=bfloat16 per_device_train_batch_size=1 gradient_accumulation_steps=1
 
 ACCELERATE_OPT=""
@@ -149,12 +148,12 @@ cmd="$cmd --output_dir ${OUTPUT_DIR}"
 
 # @@@ahoaho XXX
 #cmd="$cmd --num_generations 4"  # default: 8. The effective batch size (num_processes * per_device_batch_size * gradient_accumulation_steps) must be evenly divisible by this value.
-#cmd="$cmd --num_generations 8"  # default: 8. The effective batch size (num_processes * per_device_batch_size * gradient_accumulation_steps) must be evenly divisible by this value.  # GRPO OK? for g418b num_processes=4 per_device_train_batch_size=2 num_generations=8, GRPO OK? for q306b num_processes=4 per_device_train_batch_size=4 num_generations=8, GRPO OK? for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8, GRPO AGENT OK for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8, GRPO AGENT OK for g413b use_vllm=True
+#cmd="$cmd --num_generations 8"  # default: 8. The effective batch size (num_processes * per_device_batch_size * gradient_accumulation_steps) must be evenly divisible by this value.  # GRPO OK? for g418b num_processes=4 per_device_train_batch_size=2 num_generations=8, GRPO OK? for q306b num_processes=4 per_device_train_batch_size=4 num_generations=8, GRPO OK? for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8, GRPO AGENT OK for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8, GRPO AGENT OK for g413b use_vllm=True, GRPO AGENT OK for g418b use_vllm=True num_processes=8 per_device_train_batch_size=8 num_generations=8 vllm_gpu_memory_utilization=0.5 vllm_max_model_length=65536
 # @@@ahoaho XXX
 #cmd="$cmd --per_device_train_batch_size 1"  # default: 8  # DPO OK for g4hs
 #cmd="$cmd --per_device_train_batch_size 2"  # default: 8  # GRPO OK? for g418b num_processes=4 per_device_train_batch_size=2 num_generations=8
 #cmd="$cmd --per_device_train_batch_size 4"  # default: 8  # GRPO OK? for q306b num_processes=4 per_device_train_batch_size=4 num_generations=8
-#cmd="$cmd --per_device_train_batch_size 8"  # default: 8  # GRPO OK? for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8, GRPO AGENT OK for g413b use_vllm=True
+#cmd="$cmd --per_device_train_batch_size 8"  # default: 8  # GRPO OK? for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8, GRPO AGENT OK for g413b use_vllm=True, GRPO AGENT OK for g418b use_vllm=True num_processes=8 per_device_train_batch_size=8 num_generations=8 vllm_gpu_memory_utilization=0.5 vllm_max_model_length=65536
 #cmd="$cmd --num_train_epochs 1"  # default: 3
 # @@@ahoaho XXX WIP for functional test
 ####cmd="$cmd --save_strategy epoch"  # default: steps
@@ -181,7 +180,10 @@ cmd="$cmd --bf16 True"
 #cmd="$cmd --eval_strategy steps"  # default: no  # requires test split
 #cmd="$cmd --eval_steps 50"
 #cmd="$cmd --per_device_eval_batch_size 1"  # default: 8
-cmd="$cmd --use_vllm True"  # default: False (see. trl/trainer/grpo_config.py)  # GRPO OK for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8 `bsub -gpu num=4:mode=shared:j_exclusive=yes:gmodel=XXX ...` (NOTE: `bsub -gpu num=4:mode=exclusive_process:gmodel=XXX ...` causes CUDA warning: CUDA-capable device(s) is/are busy or unavailable (function destroyEvent)), GRPO OK? for g413b(preemptable) save_strategy=steps save_steps=50 resume_from_checkpoint=True use_vllm=True, GRPO AGENT OK for g413b use_vllm=True
+cmd="$cmd --use_vllm True"  # default: False (see. trl/trainer/grpo_config.py)  # GRPO OK for q306b num_processes=4 per_device_train_batch_size=8 num_generations=8 `bsub -gpu num=4:mode=shared:j_exclusive=yes:gmodel=XXX ...` (NOTE: `bsub -gpu num=4:mode=exclusive_process:gmodel=XXX ...` causes CUDA warning: CUDA-capable device(s) is/are busy or unavailable (function destroyEvent)), GRPO OK? for g413b(preemptable) save_strategy=steps save_steps=50 resume_from_checkpoint=True use_vllm=True, GRPO AGENT OK for g413b use_vllm=True, GRPO AGENT OK for g418b use_vllm=True num_processes=8 per_device_train_batch_size=8 num_generations=8 vllm_gpu_memory_utilization=0.5 vllm_max_model_length=65536
+# @@@ahoaho XXX for GRPO AGENT OK for g418b
+#cmd="$cmd --vllm_gpu_memory_utilization 0.5"  # default: 0.3  # GRPO AGENT OK for g418b use_vllm=True num_processes=8 per_device_train_batch_size=8 num_generations=8 vllm_gpu_memory_utilization=0.5 vllm_max_model_length=65536
+#cmd="$cmd --vllm_max_model_length 65536"  # default: model's max seq len (see "max_position_embeddings" value in config.json)  # GRPO AGENT OK for g418b use_vllm=True num_processes=8 per_device_train_batch_size=8 num_generations=8 vllm_gpu_memory_utilization=0.5 vllm_max_model_length=65536
 echo "$cmd" | tee -a ${LOGFILE}
 eval "$cmd" 2>&1 | tee -a ${LOGFILE}
 
